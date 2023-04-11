1921b80a60e1cb0e6f59ed17db619658ed45a776
rie.n <x1111.rie@gmail.com>
2023-04-12 00:43:23 +0900

    second commit

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,8 +1,25 @@
 // import { getDefaultNormalizer } from '@testing-library/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SignOut from './SignOut';
+import { db } from "../firebase";
+
 
 function Line() {
-  return <div> Lineです。</div>;
+  const [messages, setMessages] = useState();
+  useEffect(() => {
+    db.collection("messages")
+    .orderBy("createdAt");
+    .limit(50)
+    .onSnapshot((snapshot) => {
+      setMessages(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+  return(
+    <div>
+      {console.log(messages)}
+      <SignOut />
+    </div>
+  );
 }
 
 export default Line;
